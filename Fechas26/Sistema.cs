57d6891906ee99a52c2bfb5778d915d6389075bd
JoelCAI <joelargentina12345@gmail.com>
2022-04-14 01:01:48 -0300

    Agregar archivos de proyecto.

---
+++ Fechas26/Sistema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas26
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+            Console.WriteLine("\n Recuerde seguir las instrucciones. Presione cualquier tecla para continuar");
+            Console.ReadKey();
+            DateTime fecha = Validador.ValidarFechaIngresada("\n Ingrese una Fecha");
+
+            Console.Clear();
+            Console.WriteLine("\n La fecha ingresada es: " + fecha.ToLongDateString());
+   
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
